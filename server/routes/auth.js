d19ad9bf0b795567f3017b7855fce5790fa88abd
Chai Yu Pai <fantasyatelier@gmail.com>
2013-10-07 14:45:21 +0800

    #3 Auth Server

---
+++ server/routes/auth.js
@@ -0,0 +1,28 @@
+var config = require("../config.json"),
+    crypto = require("crypto"),
+    mysql = require("mysql"),
+    conn = mysql.createConnection({
+      host: config.host,
+      user: config.account,
+      password: config.password
+    });
+
+console.dir (config)
+conn.connect();
+
+exports.login = function (req, res) {
+  var account, password;
+
+  account = req.body.account;
+  password = req.body.password;
+
+  conn.query("SELECT password, salt FROM member WHERE account = ?", [account], function(err, row, field){
+    if (err) throw err;
+
+    if (row.length) {
+      console.dir(row)
+    }
+
+  })
+
+}
