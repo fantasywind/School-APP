1df124e90837b5cf17426dcc7b6b8e6c3d707a06
Chai Yu Pai <fantasyatelier@gmail.com>
2013-10-08 03:36:16 +0800

    #3 訪客登入前端 / 自動登入系統

--- www/js/login.js
+++ www/js/login.js
@@ -1,46 +1,82 @@
-$(".login-form").delegate('.actions > button', 'click', function (e){
-  var loginForm = $(e.delegateTarget),
-      stuNum = $(loginForm).find("input[name='student_id']").val(),
-      stuPass = $(loginForm).find("input[name='passwd']").val(),
-      anonyMail = $(loginForm).find("input[name='mail']").val();
+document.addEventListener('deviceready', function(){
+  $("form").on('submit autologin', function (e){
+    e.preventDefault(); 
+    var stuNum = $("input[name='student_id']").val(),
+        stuPass = $("input[name='passwd']").val(),
+        anonyMail = $("input[name='mail']").val();
 
-  if (stuNum != '' && stuPass != '') {
-    $.ajax(SERVER + "login", {
-      method: 'POST',
-      dataType: 'json',
-      data: {
-        account: stuNum,
-        password: stuPass
-      },
-      success: function (result) {
-        switch (result.status) {
-          case 'logined':
-            location.href = "index.html";
-            break;
-          case 'not found':
-            alert('找不到帳號! 請確認後再登入');
-            break;
-          case 'failed':
-            alert('密碼錯誤!');
-            break;
-          default:
-            alert('系統異常，請回報相關單位或檢查您的網路連線');
-            break;
+    if (stuNum != '' && stuPass != '') {
+      $.ajax(SERVER + "login", {
+        type: 'POST',
+        dataType: 'json',
+        data: {
+          account: stuNum,
+          password: stuPass
+        },
+        success: function (result) {
+          $("#autologin").fadeOut(function () {
+            $(this).remove();
+          });
+          switch (result.status) {
+            case 'logined':
+              location.href = "index.html";
+              localStorage.mode = 'normal';
+              localStorage.account = stuNum;
+              localStorage.password = stuPass;
+              break;
+            case 'not found':
+              alert('找不到帳號! 請確認後再登入');
+              break;
+            case 'failed':
+              alert('密碼錯誤!');
+              break;
+            default:
+              alert('系統異常，請回報相關單位或檢查您的網路連線');
+              break;
+          }
+        },
+        error: function (err) {
+          alert('系統異常，請回報相關單位或檢查您的網路連線');
         }
-      }
-    });
-  } else if (anonyMail != ''){
-    $.ajax(SERVER + "login/anonymous", {
-      method: 'POST',
-      dataType: 'json',
-      data: {
-        mail: anonyMail
-      },
-      success: function (result) {
-        
-      }
-    });
-  } else {
-    console.info('Please input login data.')
+      });
+    } else if (anonyMail != ''){
+      $.ajax(SERVER + "anonymous", {
+        type: 'POST',
+        dataType: 'json',
+        data: {
+          mail: anonyMail
+        },
+        success: function (result) {
+          switch (result.status) {
+            case 'added':
+            case 'existed':
+              location.href = "index.html";
+              localStorage.mode = 'anonymous'
+              localStorage.anonymous = anonyMail
+              break;
+            default:
+              alert('系統異常，請回報相關單位或檢查您的網路連線');
+              break;
+          }
+        },
+        error: function (err) {
+          alert('系統異常，請回報相關單位或檢查您的網路連線');
+        }
+      });
+    } else {
+      console.info('Please input login data.')
+    }
+    return false;
+  });
+
+  if (localStorage.account && localStorage.password) {
+    $("<div id='autologin'>自動登入中</div>").appendTo(document.body);
+    $("input[name='student_id']").val(localStorage.account);
+    $("input[name='passwd']").val(localStorage.password);
+    $("form").trigger('autologin');
+  } else if (localStorage.anonymous) {
+    $("<div id='autologin'>自動登入中</div>").appendTo(document.body);
+    $("input[name='mail']").val(localStorage.anonymous);
+    $("form").trigger('autologin');
   }
 });
